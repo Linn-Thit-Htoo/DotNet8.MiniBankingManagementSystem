57794b74ba655fca0a7adc7f1b6e4f9782161f86
Linn Thit <lth1212001@gmail.com>
2024-05-16 20:46:10 +0630

    Add project files.

---
+++ DotNet8.MiniBankingManagementSystem.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.MiniBankingManagementSystem.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ DotNet8.MiniBankingManagementSystem.Api/Program.cs
@@ -0,0 +1,30 @@
+using DotNet8.MiniBankingManagementSystem.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
