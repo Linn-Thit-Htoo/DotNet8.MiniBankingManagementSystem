73ffabc9b49f1a87f648a7f48b24bc038ebd59bb
Linn Thit <lth1212001@gmail.com>
2024-05-17 19:10:42 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Setup/TransactionHistory/TransactionRequestModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Setup/TransactionHistory/TransactionRequestModel.cs
@@ -1,9 +1,8 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.TransactionHistory
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.TransactionHistory;
+
+public class TransactionRequestModel
 {
-    public class TransactionRequestModel
-    {
-        public string FromAccountNo { get; set; } = null!;
-        public string ToAccountNo { get; set; } = null!;
-        public decimal Amount { get; set; }
-    }
+    public string FromAccountNo { get; set; } = null!;
+    public string ToAccountNo { get; set; } = null!;
+    public decimal Amount { get; set; }
 }
