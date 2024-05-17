01f423fb5f346a2a4f09cab9b42aae7987d2a4c3
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:21:06 +0630

    Cleaning Code

--- DotNet8.MiniBankingManagementSystem.Api/Features/State/BL_State.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/State/BL_State.cs
@@ -4,12 +4,16 @@ namespace DotNet8.MiniBankingManagementSystem.Api.Features.State
 {
     public class BL_State
     {
+        #region Initializations
+
         private readonly DA_State _dA_State;
 
         public BL_State(DA_State dA_State)
         {
             _dA_State = dA_State;
         }
+
+        #endregion
 
         #region GetStateListAsync
 
