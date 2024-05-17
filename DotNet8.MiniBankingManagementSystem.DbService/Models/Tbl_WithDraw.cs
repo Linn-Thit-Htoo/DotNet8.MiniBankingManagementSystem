2324b9675cbd2f1b98e402de92595c4485f9dfa9
Linn Thit <lth1212001@gmail.com>
2024-05-17 16:48:46 +0630

    -

--- DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_WithDraw.cs
+++ DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_WithDraw.cs
@@ -6,15 +6,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNet8.MiniBankingManagementSystem.DbService.Models
+namespace DotNet8.MiniBankingManagementSystem.DbService.Models;
+
+[Table("WithDraw")]
+public class Tbl_WithDraw
 {
-    [Table("WithDraw")]
-    public class Tbl_WithDraw
-    {
-        [Key]
-        public long WithDrawId { get; set; }
-        public string AccountNo { get; set; } = null!;
-        public decimal Amount { get; set; }
-        public DateTime WithDrawDate { get; set; }
-    }
+    [Key]
+    public long WithDrawId { get; set; }
+    public string AccountNo { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public DateTime WithDrawDate { get; set; }
 }
