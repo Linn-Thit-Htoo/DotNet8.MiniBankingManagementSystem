1eb46549fe7dc80eb78cf34e2093dffb00ec861a
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:25:49 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/Features/Township/BL_Township.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/Township/BL_Township.cs
@@ -1,25 +1,24 @@
 ﻿using DotNet8.MiniBankingManagementSystem.Models.Setup.Township;
 
-namespace DotNet8.MiniBankingManagementSystem.Api.Features.Township
+namespace DotNet8.MiniBankingManagementSystem.Api.Features.Township;
+
+public class BL_Township
 {
-    public class BL_Township
+    private readonly DA_Township _dA_Township;
+
+    public BL_Township(DA_Township dA_Township)
     {
-        private readonly DA_Township _dA_Township;
+        _dA_Township = dA_Township;
+    }
 
-        public BL_Township(DA_Township dA_Township)
-        {
-            _dA_Township = dA_Township;
-        }
+    public async Task<TownshipResponseModel> GetTownshipListAsync()
+    {
+        return await _dA_Township.GetTownshipListAsync();
+    }
 
-        public async Task<TownshipResponseModel> GetTownshipListAsync()
-        {
-            return await _dA_Township.GetTownshipListAsync();
-        }
-
-        public async Task<int> CreateTownshipListAsync()
-        {
-            int result = await _dA_Township.CreateTownshipListAsync();
-            return result;
-        }
+    public async Task<int> CreateTownshipListAsync()
+    {
+        int result = await _dA_Township.CreateTownshipListAsync();
+        return result;
     }
 }
