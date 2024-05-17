676b5739f1ff966fe39c228228351d099c097e83
Linn Thit <lth1212001@gmail.com>
2024-05-17 16:48:26 +0630

    -

--- DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_Deposit.cs
+++ DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_Deposit.cs
@@ -1,15 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DotNet8.MiniBankingManagementSystem.DbService.Models
+namespace DotNet8.MiniBankingManagementSystem.DbService.Models;
+
+[Table("Deposit")]
+public class Tbl_Deposit
 {
-    [Table("Deposit")]
-    public class Tbl_Deposit
-    {
-        [Key]
-        public long DepositId { get; set; }
-        public string AccountNo { get; set; } = null!;
-        public decimal Amount { get; set; }
-        public DateTime DepositDate { get; set; }
-    }
+    [Key]
+    public long DepositId { get; set; }
+    public string AccountNo { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public DateTime DepositDate { get; set; }
 }
