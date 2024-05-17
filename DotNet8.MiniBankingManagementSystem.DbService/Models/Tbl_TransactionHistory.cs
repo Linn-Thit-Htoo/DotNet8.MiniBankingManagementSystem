6cd141a76c7ea40571be310579b32e96b9d696bf
Linn Thit <lth1212001@gmail.com>
2024-05-17 19:08:38 +0630

    -

--- DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_TransactionHistory.cs
+++ DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_TransactionHistory.cs
@@ -1,16 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DotNet8.MiniBankingManagementSystem.DbService.Models
+namespace DotNet8.MiniBankingManagementSystem.DbService.Models;
+
+[Table("TransactionHistory")]
+public class Tbl_TransactionHistory
 {
-    [Table("TransactionHistory")]
-    public class Tbl_TransactionHistory
-    {
-        [Key]
-        public long TransactionHistoryId { get; set; }
-        public string FromAccountNo { get; set; } = null!;
-        public string ToAccountNo { get; set; } = null!;
-        public decimal Amount { get; set; }
-        public DateTime TransactionDate { get; set; }
-    }
+    [Key]
+    public long TransactionHistoryId { get; set; }
+    public string FromAccountNo { get; set; } = null!;
+    public string ToAccountNo { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public DateTime TransactionDate { get; set; }
 }
