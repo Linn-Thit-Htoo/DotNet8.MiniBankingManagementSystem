1716e85e26d703817a47ab03238344df99377286
Linn Thit <lth1212001@gmail.com>
2024-05-17 19:09:26 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Setup/TransactionHistory/TransactionHistoryModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Setup/TransactionHistory/TransactionHistoryModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.TransactionHistory
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.TransactionHistory;
+
+public class TransactionHistoryModel
 {
-    public class TransactionHistoryModel
-    {
-        public long TransactionHistoryId { get; set; }
-        public string FromAccountNo { get; set; } = null!;
-        public string ToAccountNo { get; set; } = null!;
-        public decimal Amount { get; set; }
-        public DateTime TransactionDate { get; set; }
-    }
+    public long TransactionHistoryId { get; set; }
+    public string FromAccountNo { get; set; } = null!;
+    public string ToAccountNo { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public DateTime TransactionDate { get; set; }
 }
