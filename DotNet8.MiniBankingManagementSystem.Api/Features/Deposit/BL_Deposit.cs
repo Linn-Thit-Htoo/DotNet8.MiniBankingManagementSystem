892defb4e51c6845cc2c5afbe2eeef6d2bab6ab4
Linn Thit <lth1212001@gmail.com>
2024-05-17 16:45:38 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/Features/Deposit/BL_Deposit.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/Deposit/BL_Deposit.cs
@@ -1,34 +1,33 @@
 ﻿using DotNet8.MiniBankingManagementSystem.Models.Setup.Deposit;
 
-namespace DotNet8.MiniBankingManagementSystem.Api.Features.Deposit
+namespace DotNet8.MiniBankingManagementSystem.Api.Features.Deposit;
+
+public class BL_Deposit
 {
-    public class BL_Deposit
+    private readonly DA_Deposit _dA_Deposit;
+
+    public BL_Deposit(DA_Deposit dA_Deposit)
     {
-        private readonly DA_Deposit _dA_Deposit;
+        _dA_Deposit = dA_Deposit;
+    }
 
-        public BL_Deposit(DA_Deposit dA_Deposit)
-        {
-            _dA_Deposit = dA_Deposit;
-        }
+    public async Task<DepositListResponseModel> GetDepositListByAccountNoAsync(string accountNo)
+    {
+        if (string.IsNullOrWhiteSpace(accountNo))
+            throw new Exception("Account No cannot be empty.");
 
-        public async Task<DepositListResponseModel> GetDepositListByAccountNoAsync(string accountNo)
-        {
-            if (string.IsNullOrWhiteSpace(accountNo))
-                throw new Exception("Account No cannot be empty.");
+        return await _dA_Deposit.GetDepositListByAccountNoAsync(accountNo);
+    }
 
-            return await _dA_Deposit.GetDepositListByAccountNoAsync(accountNo);
-        }
+    public async Task<bool> CreateDepositAsync(DepositRequestModel requestModel)
+    {
+        if (string.IsNullOrWhiteSpace(requestModel.AccountNo))
+            throw new Exception("Account No cannot be empty.");
 
-        public async Task<bool> CreateDepositAsync(DepositRequestModel requestModel)
-        {
-            if (string.IsNullOrWhiteSpace(requestModel.AccountNo))
-                throw new Exception("Account No cannot be empty.");
+        if (requestModel.Amount <= 0)
+            throw new Exception("Amount is invalid.");
 
-            if (requestModel.Amount <= 0)
-                throw new Exception("Amount is invalid.");
-
-            bool isSuccess = await _dA_Deposit.CreateDepositAsync(requestModel);
-            return isSuccess;
-        }
+        bool isSuccess = await _dA_Deposit.CreateDepositAsync(requestModel);
+        return isSuccess;
     }
 }
