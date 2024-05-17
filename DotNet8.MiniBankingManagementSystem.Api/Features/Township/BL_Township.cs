b200c7583655443bfdf9d781405fbe82834bdc94
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:26:34 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/Features/Township/BL_Township.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/Township/BL_Township.cs
@@ -4,6 +4,7 @@ namespace DotNet8.MiniBankingManagementSystem.Api.Features.Township;
 
 public class BL_Township
 {
+
     private readonly DA_Township _dA_Township;
 
     public BL_Township(DA_Township dA_Township)
