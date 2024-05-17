223a687b5df0d3f9daab35492885c069d4634396
Linn Thit <lth1212001@gmail.com>
2024-05-17 19:07:52 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/Features/TransactionHistory/TransactionHistoryController.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/TransactionHistory/TransactionHistoryController.cs
@@ -1,43 +1,42 @@
 ﻿using DotNet8.MiniBankingManagementSystem.Models.Setup.TransactionHistory;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.MiniBankingManagementSystem.Api.Features.TransactionHistory
+namespace DotNet8.MiniBankingManagementSystem.Api.Features.TransactionHistory;
+
+[Route("api/v1/transaction-histories")]
+[ApiController]
+public class TransactionHistoryController : ControllerBase
 {
-    [Route("api/v1/transaction-histories")]
-    [ApiController]
-    public class TransactionHistoryController : ControllerBase
+    private readonly BL_TransactionHistory _bL_TransactionHistory;
+
+    public TransactionHistoryController(BL_TransactionHistory bL_TransactionHistory)
     {
-        private readonly BL_TransactionHistory _bL_TransactionHistory;
+        _bL_TransactionHistory = bL_TransactionHistory;
+    }
 
-        public TransactionHistoryController(BL_TransactionHistory bL_TransactionHistory)
+    [HttpGet]
+    public async Task<IActionResult> GetTransactionListByAccountNo(string accountNo)
+    {
+        try
         {
-            _bL_TransactionHistory = bL_TransactionHistory;
+            return Ok(await _bL_TransactionHistory.GetTransactionHistoryListByAccountNoAsync(accountNo));
         }
-
-        [HttpGet]
-        public async Task<IActionResult> GetTransactionListByAccountNo(string accountNo)
+        catch (Exception ex)
         {
-            try
-            {
-                return Ok(await _bL_TransactionHistory.GetTransactionHistoryListByAccountNoAsync(accountNo));
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateTransaction([FromBody] TransactionRequestModel requestModel)
+    [HttpPost]
+    public async Task<IActionResult> CreateTransaction([FromBody] TransactionRequestModel requestModel)
+    {
+        try
         {
-            try
-            {
-                return await _bL_TransactionHistory.CreateTransactionAsync(requestModel) ? StatusCode(201, "Successful.") : BadRequest("Fail.");
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return await _bL_TransactionHistory.CreateTransactionAsync(requestModel) ? StatusCode(201, "Successful.") : BadRequest("Fail.");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }
