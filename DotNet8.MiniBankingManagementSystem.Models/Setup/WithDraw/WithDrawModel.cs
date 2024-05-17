7ec52162b7550abf81138414b211cc24e8917cfa
Linn Thit <lth1212001@gmail.com>
2024-05-17 16:52:15 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Setup/WithDraw/WithDrawModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Setup/WithDraw/WithDrawModel.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.WithDraw
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.WithDraw;
+
+public class WithDrawModel
 {
-    public class WithDrawModel
-    {
-        public long WithDrawId { get; set; }
-        public string AccountNo { get; set; } = null!;
-        public decimal Amount { get; set; }
-        public DateTime WithDrawDate { get; set; }
-    }
+    public long WithDrawId { get; set; }
+    public string AccountNo { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public DateTime WithDrawDate { get; set; }
 }
