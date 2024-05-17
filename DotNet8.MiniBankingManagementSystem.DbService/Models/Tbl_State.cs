32803a004af0d7342666020f14a2a713818ae073
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:34:07 +0630

    -

--- DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_State.cs
+++ DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_State.cs
@@ -1,14 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DotNet8.MiniBankingManagementSystem.DbService.Models
+namespace DotNet8.MiniBankingManagementSystem.DbService.Models;
+
+[Table("State")]
+public class Tbl_State
 {
-    [Table("State")]
-    public class Tbl_State
-    {
-        [Key]
-        public long StateId { get; set; }
-        public string StateCode { get; set; } = null!;
-        public string StateName { get; set; } = null!;
-    }
+    [Key]
+    public long StateId { get; set; }
+    public string StateCode { get; set; } = null!;
+    public string StateName { get; set; } = null!;
 }
