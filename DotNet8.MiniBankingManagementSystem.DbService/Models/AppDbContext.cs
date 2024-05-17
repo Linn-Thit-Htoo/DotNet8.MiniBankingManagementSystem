5bf1e912b38ab35d7353bed57cdb7ae92d30c21c
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:33:22 +0630

    -

--- DotNet8.MiniBankingManagementSystem.DbService/Models/AppDbContext.cs
+++ DotNet8.MiniBankingManagementSystem.DbService/Models/AppDbContext.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.MiniBankingManagementSystem.DbService.Models
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
-        }
+namespace DotNet8.MiniBankingManagementSystem.DbService.Models;
 
-        public DbSet<Tbl_Account> Tbl_Account { get; set; }
-        public DbSet<Tbl_State> Tbl_State { get; set; }
-        public DbSet<Tbl_Township> Tbl_Township { get; set; }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
     }
+
+    public DbSet<Tbl_Account> Tbl_Account { get; set; }
+    public DbSet<Tbl_State> Tbl_State { get; set; }
+    public DbSet<Tbl_Township> Tbl_Township { get; set; }
 }
