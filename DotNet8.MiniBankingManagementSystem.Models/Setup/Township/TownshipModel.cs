ad79c6bf3ca2eea64427462b79ebf269f5937607
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:36:06 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Setup/Township/TownshipModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Setup/Township/TownshipModel.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.Township
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.Township;
+
+public class TownshipModel
 {
-    public class TownshipModel
-    {
-        public long TownshipId { get; set; }
-        public string TownshipCode { get; set; } = null!;
-        public string TownshipName { get; set; } = null!;
-        public string StateCode { get; set; } = null!;
-    }
+    public long TownshipId { get; set; }
+    public string TownshipCode { get; set; } = null!;
+    public string TownshipName { get; set; } = null!;
+    public string StateCode { get; set; } = null!;
 }
