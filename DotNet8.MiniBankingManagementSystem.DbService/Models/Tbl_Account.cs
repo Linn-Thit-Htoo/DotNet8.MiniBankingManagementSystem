f6f7dfa262e99ba6f815c99470942181962a72f9
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:33:43 +0630

    -

--- DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_Account.cs
+++ DotNet8.MiniBankingManagementSystem.DbService/Models/Tbl_Account.cs
@@ -1,20 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DotNet8.MiniBankingManagementSystem.DbService.Models
+namespace DotNet8.MiniBankingManagementSystem.DbService.Models;
+
+[Table("Account")]
+public class Tbl_Account
 {
-    [Table("Account")]
-    public class Tbl_Account
-    {
-        [Key]
-        public long AccountId { get; set; }
-        public string AccountNo { get; set; } = null!;
-        public decimal Balance { get; set; }
-        public decimal AccountLevel { get; set; }
-        public string CustomerCode { get; set; } = null!;
-        public string CustomerName { get; set; } = null!;
-        public string StateCode { get; set; } = null!;
-        public string TownshipCode { get; set; } = null!;
-        public bool IsActive { get; set; }
-    }
+    [Key]
+    public long AccountId { get; set; }
+    public string AccountNo { get; set; } = null!;
+    public decimal Balance { get; set; }
+    public decimal AccountLevel { get; set; }
+    public string CustomerCode { get; set; } = null!;
+    public string CustomerName { get; set; } = null!;
+    public string StateCode { get; set; } = null!;
+    public string TownshipCode { get; set; } = null!;
+    public bool IsActive { get; set; }
 }
