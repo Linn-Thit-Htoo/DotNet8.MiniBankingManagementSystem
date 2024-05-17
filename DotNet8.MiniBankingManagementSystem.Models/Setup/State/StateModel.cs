e844d38452cc68abc5b8186dd1b070614ef56c0a
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:35:35 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Setup/State/StateModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Setup/State/StateModel.cs
@@ -1,9 +1,8 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.State
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.State;
+
+public class StateModel
 {
-    public class StateModel
-    {
-        public long StateId { get; set; }
-        public string StateCode { get; set; } = null!;
-        public string StateName { get; set; } = null!;
-    }
+    public long StateId { get; set; }
+    public string StateCode { get; set; } = null!;
+    public string StateName { get; set; } = null!;
 }
