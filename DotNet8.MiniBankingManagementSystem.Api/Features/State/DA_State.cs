ed8103896d9955197fdbc8a6fd172cae55aa36b5
Linn Thit <lth1212001@gmail.com>
2024-05-17 12:23:09 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/Features/State/DA_State.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/State/DA_State.cs
@@ -4,52 +4,51 @@ using DotNet8.MiniBankingManagementSystem.Models.Setup.State;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
-namespace DotNet8.MiniBankingManagementSystem.Api.Features.State
+namespace DotNet8.MiniBankingManagementSystem.Api.Features.State;
+
+public class DA_State
 {
-    public class DA_State
+    #region Initializations
+
+    private readonly AppDbContext _appDbContext;
+
+    public DA_State(AppDbContext appDbContext)
     {
-        #region Initializations
-
-        private readonly AppDbContext _appDbContext;
-
-        public DA_State(AppDbContext appDbContext)
-        {
-            _appDbContext = appDbContext;
-        }
-
-        #endregion
-
-        #region GetStateListAsync
-
-        public async Task<StateListResponseModel> GetStateListAsync()
-        {
-            var states = await _appDbContext.Tbl_State
-                .AsNoTracking()
-                .OrderByDescending(x => x.StateId)
-                .ToListAsync();
-
-            var lst = states.Select(x => x.Change()).ToList();
-
-            return new StateListResponseModel
-            {
-                DataLst = lst
-            };
-        }
-
-        #endregion
-
-        #region CreateStatesAsync
-
-        public async Task<int> CreateStatesAsync()
-        {
-            string jsonStr = await File.ReadAllTextAsync("Data/StateList.json");
-            List<Tbl_State> lst = JsonConvert.DeserializeObject<List<Tbl_State>>(jsonStr)!;
-            await _appDbContext.Tbl_State.AddRangeAsync(lst);
-            int result = await _appDbContext.SaveChangesAsync();
-
-            return result;
-        }
-
-        #endregion
+        _appDbContext = appDbContext;
     }
+
+    #endregion
+
+    #region GetStateListAsync
+
+    public async Task<StateListResponseModel> GetStateListAsync()
+    {
+        var states = await _appDbContext.Tbl_State
+            .AsNoTracking()
+            .OrderByDescending(x => x.StateId)
+            .ToListAsync();
+
+        var lst = states.Select(x => x.Change()).ToList();
+
+        return new StateListResponseModel
+        {
+            DataLst = lst
+        };
+    }
+
+    #endregion
+
+    #region CreateStatesAsync
+
+    public async Task<int> CreateStatesAsync()
+    {
+        string jsonStr = await File.ReadAllTextAsync("Data/StateList.json");
+        List<Tbl_State> lst = JsonConvert.DeserializeObject<List<Tbl_State>>(jsonStr)!;
+        await _appDbContext.Tbl_State.AddRangeAsync(lst);
+        int result = await _appDbContext.SaveChangesAsync();
+
+        return result;
+    }
+
+    #endregion
 }
