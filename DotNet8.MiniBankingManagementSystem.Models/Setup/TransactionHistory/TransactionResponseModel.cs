eb0ad98d281c9471a5b53dca7a6651eca79639dc
Linn Thit <lth1212001@gmail.com>
2024-05-19 18:14:13 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Setup/TransactionHistory/TransactionResponseModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Setup/TransactionHistory/TransactionResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.MiniBankingManagementSystem.Models.Setup.TransactionHistory
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Setup.TransactionHistory
 {
     public class TransactionResponseModel
     {
