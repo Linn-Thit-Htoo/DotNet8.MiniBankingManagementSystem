ce4d617642ce2a589e1abb648fd6295fa8d2a704
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-13 10:20:53 +0630

    -

---
+++ DotNet8.MiniBankingManagementSystem.Modules/Features/Account/BL_Account.cs
@@ -0,0 +1,51 @@
+﻿using DotNet8.MiniBankingManagementSystem.Models.Features.Account;
+
+namespace DotNet8.MiniBankingManagementSystem.Modules.Features.Account;
+
+public class BL_Account
+{
+    #region Initializations
+
+    private readonly DA_Account _dA_Account;
+
+    public BL_Account(DA_Account dA_Account)
+    {
+        _dA_Account = dA_Account;
+    }
+
+    #endregion
+
+    #region GetAccountListAsync
+
+    public async Task<AccountListResponseModel> GetAccountListAsync()
+    {
+        return await _dA_Account.GetAccountListAsync();
+    }
+
+    #endregion
+
+    #region CreateAccount
+
+    public async Task<int> CreateAccount(AccountRequestModel requestModel)
+    {
+        if (string.IsNullOrEmpty(requestModel.CustomerName))
+            throw new Exception("Customer Name cannot be empty.");
+
+        if (requestModel.Balance <= 0)
+            throw new Exception("Balance is invalid.");
+
+        if (string.IsNullOrEmpty(requestModel.StateCode))
+            throw new Exception("State Code cannot be empty.");
+
+        if (string.IsNullOrEmpty(requestModel.TownshipCode))
+            throw new Exception("Township Code cannot be empty.");
+
+        // validate state
+
+        int result = await _dA_Account.CreateAccount(requestModel);
+
+        return result;
+    }
+
+    #endregion
+}
