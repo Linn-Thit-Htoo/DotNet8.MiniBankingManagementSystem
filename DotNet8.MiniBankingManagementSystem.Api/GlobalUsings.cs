c7873f57e39984d5bf1be0e9ead0bdd30df2d55a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-13 21:00:16 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/GlobalUsings.cs
+++ DotNet8.MiniBankingManagementSystem.Api/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿// Global using directives
 
 global using DotNet8.MiniBankingManagementSystem.DbService.Models;
-global using DotNet8.MiniBankingManagementSystem.Models;
 global using Microsoft.EntityFrameworkCore;
 global using Newtonsoft.Json;
