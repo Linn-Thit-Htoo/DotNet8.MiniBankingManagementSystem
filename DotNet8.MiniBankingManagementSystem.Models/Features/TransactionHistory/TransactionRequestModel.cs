1dd82c90cc6ef99fbf76fcd952f2fd81d20fefc0
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-13 10:26:56 +0630

    [Upgrade] Transaction History Module

--- DotNet8.MiniBankingManagementSystem.Models/Features/TransactionHistory/TransactionRequestModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Features/TransactionHistory/TransactionRequestModel.cs
@@ -1,8 +1,14 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Features.TransactionHistory;
+﻿
+namespace DotNet8.MiniBankingManagementSystem.Models.Features.TransactionHistory;
 
 public class TransactionRequestModel
 {
     public string FromAccountNo { get; set; } = null!;
     public string ToAccountNo { get; set; } = null!;
     public decimal Amount { get; set; }
+
+    public DbService.Models.TransactionHistory Change()
+    {
+        throw new NotImplementedException();
+    }
 }
