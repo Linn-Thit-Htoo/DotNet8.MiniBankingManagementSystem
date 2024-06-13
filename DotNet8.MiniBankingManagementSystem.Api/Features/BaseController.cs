e7937e523c871244bfe02822c84c3555cdfebaba
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-13 20:59:40 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/Features/BaseController.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/BaseController.cs
@@ -2,36 +2,35 @@
 using DotNet8.MiniBankingManagementSystem.Models.Resources;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.MiniBankingManagementSystem.Api.Features
+namespace DotNet8.MiniBankingManagementSystem.Api.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object? obj)
     {
-        protected IActionResult Content(object? obj)
-        {
-            return Ok(JsonConvert.SerializeObject(obj));
-        }
+        return Ok(JsonConvert.SerializeObject(obj));
+    }
 
-        protected IActionResult Accepted(EnumRespType enumRespType)
+    protected IActionResult Accepted(EnumRespType enumRespType)
+    {
+        switch (enumRespType)
         {
-            switch (enumRespType)
-            {
-                case EnumRespType.Created:
-                    return StatusCode(201, MessageResource.SaveSuccess);
-                case EnumRespType.Accepted:
-                    return StatusCode(202, MessageResource.SaveSuccess);
-                case EnumRespType.Deleted:
-                    return StatusCode(202, MessageResource.DeleteSuccess);
-                case EnumRespType.None:
-                default:
-                    return BadRequest("Invalid Response Type.");
-            }
+            case EnumRespType.Created:
+                return StatusCode(201, MessageResource.SaveSuccess);
+            case EnumRespType.Accepted:
+                return StatusCode(202, MessageResource.SaveSuccess);
+            case EnumRespType.Deleted:
+                return StatusCode(202, MessageResource.DeleteSuccess);
+            case EnumRespType.None:
+            default:
+                return BadRequest("Invalid Response Type.");
         }
+    }
 
-        protected IActionResult HandleFailure(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+    protected IActionResult HandleFailure(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
