5ef63075e09841ba35bbf6b7a53322c7fae9d002
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-13 10:23:21 +0630

    [Upgrade] Township Module

---
+++ DotNet8.MiniBankingManagementSystem.Modules/Features/Township/BL_Township.cs
@@ -0,0 +1,36 @@
+﻿using DotNet8.MiniBankingManagementSystem.Models.Features.Township;
+
+namespace DotNet8.MiniBankingManagementSystem.Modules.Features.Township;
+
+public class BL_Township
+{
+    #region Initializations
+
+    private readonly DA_Township _dA_Township;
+
+    public BL_Township(DA_Township dA_Township)
+    {
+        _dA_Township = dA_Township;
+    }
+
+    #endregion
+
+    #region GetTownshipListAsync
+
+    public async Task<TownshipResponseModel> GetTownshipListAsync()
+    {
+        return await _dA_Township.GetTownshipListAsync();
+    }
+
+    #endregion
+
+    #region CreateTownshipListAsync
+
+    public async Task<int> CreateTownshipListAsync()
+    {
+        int result = await _dA_Township.CreateTownshipListAsync();
+        return result;
+    }
+
+    #endregion
+}
---
+++ DotNet8.MiniBankingManagementSystem.Modules/Features/Township/DA_Township.cs
@@ -0,0 +1,54 @@
+﻿using DotNet8.MiniBankingManagementSystem.DbService.Models;
+using DotNet8.MiniBankingManagementSystem.Mapper;
+using DotNet8.MiniBankingManagementSystem.Models.Features.Township;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace DotNet8.MiniBankingManagementSystem.Modules.Features.Township;
+
+public class DA_Township
+{
+    #region Initializations
+
+    private readonly AppDbContext _appDbContext;
+
+    public DA_Township(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    #endregion
+
+    #region GetTownshipListAsync
+
+    public async Task<TownshipResponseModel> GetTownshipListAsync()
+    {
+        var townships = await _appDbContext.Townships
+            .AsNoTracking()
+            .OrderByDescending(x => x.TownshipId)
+            .ToListAsync();
+
+        var lst = townships.Select(x => x.Change()).ToList();
+
+        return new TownshipResponseModel
+        {
+            DataLst = lst
+        };
+    }
+
+    #endregion
+
+    #region CreateTownshipListAsync
+
+    public async Task<int> CreateTownshipListAsync()
+    {
+        string jsonStr = await File.ReadAllTextAsync("Data/TownshipList.json");
+        List<DbService.Models.Township> lst = JsonConvert.DeserializeObject<List<DbService.Models.Township>>(jsonStr)!;
+        await _appDbContext.AddRangeAsync(lst);
+        int result = await _appDbContext.SaveChangesAsync();
+
+        return result;
+    }
+
+    #endregion
+}
