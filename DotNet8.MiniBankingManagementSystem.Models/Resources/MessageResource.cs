695e4000d7e0a5803af08c3c5ed0dc0b255d1f27
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-13 20:52:03 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Resources/MessageResource.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Resources/MessageResource.cs
@@ -1,12 +1,11 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Resources
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Resources;
+
+public static class MessageResource
 {
-    public static class MessageResource
-    {
-        public static string SaveSuccess { get; } = "Saving Successful.";
-        public static string SaveFail { get; } = "Saving Fail.";
-        public static string NotFound { get; } = "No Data Found.";
-        public static string Duplicate { get; } = "Data is duplicate.";
-        public static string DeleteSuccess { get; } = "Deleting Successful.";
-        public static string DeleteFail { get; } = "Deleting Fail.";
-    }
+    public static string SaveSuccess { get; } = "Saving Successful.";
+    public static string SaveFail { get; } = "Saving Fail.";
+    public static string NotFound { get; } = "No Data Found.";
+    public static string Duplicate { get; } = "Data is duplicate.";
+    public static string DeleteSuccess { get; } = "Deleting Successful.";
+    public static string DeleteFail { get; } = "Deleting Fail.";
 }
