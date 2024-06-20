7805c1ce0e25c01312228c21c1f7ad8df154c728
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 00:39:17 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Shared/DapperService.cs
+++ DotNet8.MiniBankingManagementSystem.Shared/DapperService.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Data;
 using Dapper;
+using Microsoft.Extensions.Configuration;
 
 namespace DotNet8.MiniBankingManagementSystem.Shared;
 
@@ -14,47 +14,79 @@ public class DapperService
         _configuration = configuration;
     }
 
-    public List<T> Query<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
+    public List<T> Query<T>(
+        string query,
+        object? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
     {
-        using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DbConnection"));
+        using IDbConnection db = new SqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
         List<T> lst = db.Query<T>(query, parameters, commandType: commandType).ToList();
 
         return lst;
     }
 
-    public async Task<List<T>> QueryAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
+    public async Task<List<T>> QueryAsync<T>(
+        string query,
+        object? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
     {
-        using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DbConnection"));
+        using IDbConnection db = new SqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
         var lst = await db.QueryAsync<T>(query, parameters, commandType: commandType);
 
         return lst.ToList();
     }
 
-    public T? QueryFirstOrDefault<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
+    public T? QueryFirstOrDefault<T>(
+        string query,
+        object? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
     {
-        using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DbConnection"));
+        using IDbConnection db = new SqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
         T? item = db.Query<T>(query, parameters, commandType: commandType).FirstOrDefault();
 
         return item;
     }
 
-    public async Task<T?> QueryFirstOrDefaultAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
+    public async Task<T?> QueryFirstOrDefaultAsync<T>(
+        string query,
+        object? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
     {
-        using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DbConnection"));
-        var item = await db.QueryFirstOrDefaultAsync<T>(query, parameters, commandType: commandType);
+        using IDbConnection db = new SqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
+        var item = await db.QueryFirstOrDefaultAsync<T>(
+            query,
+            parameters,
+            commandType: commandType
+        );
 
         return item;
     }
 
     public int Execute(string query, object? parameters = null)
     {
-        using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DbConnection"));
+        using IDbConnection db = new SqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
         return db.Execute(query, parameters);
     }
 
     public async Task<int> ExecuteAsync(string query, object? parameters = null)
     {
-        using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DbConnection"));
+        using IDbConnection db = new SqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
         return await db.ExecuteAsync(query, parameters);
     }
 }
