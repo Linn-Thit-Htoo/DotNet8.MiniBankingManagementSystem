c3e8b24e4decb9244fd58cef41739b1509c064c6
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 00:38:42 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Features/TransactionHistory/TransactionHistoryReportModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Features/TransactionHistory/TransactionHistoryReportModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8.MiniBankingManagementSystem.Models.Features.TransactionHistory
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Features.TransactionHistory;
+
+public class TransactionHistoryReportModel
 {
-    public class TransactionHistoryReportModel
-    {
-        public string TransactionHistoryId { get; set; } = null!;
-        public decimal Amount { get; set; }
-        public DateTime TransactionDate { get; set; }
-        public string SenderName { get; set; } = null!;
-        public string ReceiverName { get; set; } = null!;
-    }
+    public string TransactionHistoryId { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public DateTime TransactionDate { get; set; }
+    public string SenderName { get; set; } = null!;
+    public string ReceiverName { get; set; } = null!;
 }
