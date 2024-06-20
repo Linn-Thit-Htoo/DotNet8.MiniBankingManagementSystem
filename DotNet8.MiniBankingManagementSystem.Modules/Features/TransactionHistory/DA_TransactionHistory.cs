795c0b615bc40a79c3b3871c5f3ee90e0733f273
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-20 23:26:40 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Modules/Features/TransactionHistory/DA_TransactionHistory.cs
+++ DotNet8.MiniBankingManagementSystem.Modules/Features/TransactionHistory/DA_TransactionHistory.cs
@@ -1,13 +1,13 @@
-﻿using Dapper;
-using DotNet8.MiniBankingManagementSystem.DbService.Models;
-using DotNet8.MiniBankingManagementSystem.Models.Features.TransactionHistory;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
+using Dapper;
+using DotNet8.MiniBankingManagementSystem.DbService.Models;
 using DotNet8.MiniBankingManagementSystem.Mapper;
 using DotNet8.MiniBankingManagementSystem.Models.Features;
+using DotNet8.MiniBankingManagementSystem.Models.Features.TransactionHistory;
 using DotNet8.MiniBankingManagementSystem.Models.Resources;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DotNet8.MiniBankingManagementSystem.Modules.Features.TransactionHistory;
 
@@ -28,37 +28,36 @@ public class DA_TransactionHistory
 
     #region GetTransactionHistoryListByAccountNoAsync
 
-    public async Task<Result<TransactionHistoryListResponseModel>> GetTransactionHistoryListByAccountNoAsync(string accountNo)
+    public async Task<
+        Result<TransactionHistoryListResponseModel>
+    > GetTransactionHistoryListByAccountNoAsync(string accountNo)
     {
         Result<TransactionHistoryListResponseModel> responseModel;
         try
         {
-            using IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DbConnection"));
-            var parameters = new
-            {
-                FromAccountNo = accountNo
-            };
+            using IDbConnection db = new SqlConnection(
+                _configuration.GetConnectionString("DbConnection")
+            );
+            var parameters = new { FromAccountNo = accountNo };
 
-            IEnumerable<TransactionDataModel> dataLst = await db
-                .QueryAsync<TransactionDataModel>(
+            IEnumerable<TransactionDataModel> dataLst = await db.QueryAsync<TransactionDataModel>(
                 "Sp_GetTransactionHistoryListByAccountNo",
                 parameters,
-                commandType: CommandType.StoredProcedure);
+                commandType: CommandType.StoredProcedure
+            );
 
-            var lst = dataLst.Select(x => new TransactionDataModel()
-            {
-                Amount = Math.Round(x.Amount),
-                ReceiverName = x.ReceiverName,
-                SenderName = x.SenderName,
-                TransactionDate = x.TransactionDate,
-                TransactionHistoryId = x.TransactionHistoryId
-            }).ToList();
+            var lst = dataLst
+                .Select(x => new TransactionDataModel()
+                {
+                    Amount = Math.Round(x.Amount),
+                    ReceiverName = x.ReceiverName,
+                    SenderName = x.SenderName,
+                    TransactionDate = x.TransactionDate,
+                    TransactionHistoryId = x.TransactionHistoryId
+                })
+                .ToList();
 
-            var model = new TransactionHistoryListResponseModel
-            {
-                DataLst = lst
-            };
-
+            var model = new TransactionHistoryListResponseModel { DataLst = lst };
 
             responseModel = Result<TransactionHistoryListResponseModel>.SuccessResult(model);
         }
@@ -74,7 +73,9 @@ public class DA_TransactionHistory
 
     #region CreateTransactionAsync
 
-    public async Task<Result<TransactionResponseModel>> CreateTransactionAsync(TransactionRequestModel requestModel)
+    public async Task<Result<TransactionResponseModel>> CreateTransactionAsync(
+        TransactionRequestModel requestModel
+    )
     {
         var transaction = await _appDbContext.Database.BeginTransactionAsync();
         Result<TransactionResponseModel> responseModel;
@@ -82,13 +83,15 @@ public class DA_TransactionHistory
         {
             #region Check From Account
 
-            var fromAccount = await _appDbContext.Accounts
-                .AsNoTracking()
+            var fromAccount = await _appDbContext
+                .Accounts.AsNoTracking()
                 .FirstOrDefaultAsync(x => x.AccountNo == requestModel.FromAccountNo && x.IsActive);
 
             if (fromAccount is null)
             {
-                responseModel = Result<TransactionResponseModel>.FailureResult(MessageResource.NotFound);
+                responseModel = Result<TransactionResponseModel>.FailureResult(
+                    MessageResource.NotFound
+                );
                 goto result;
             }
 
@@ -96,13 +99,15 @@ public class DA_TransactionHistory
 
             #region Check To Account
 
-            var toAccount = await _appDbContext.Accounts
-                .AsNoTracking()
+            var toAccount = await _appDbContext
+                .Accounts.AsNoTracking()
                 .FirstOrDefaultAsync(x => x.AccountNo == requestModel.ToAccountNo && x.IsActive);
 
             if (toAccount is null)
             {
-                responseModel = Result<TransactionResponseModel>.FailureResult(MessageResource.NotFound);
+                responseModel = Result<TransactionResponseModel>.FailureResult(
+                    MessageResource.NotFound
+                );
                 goto result;
             }
 
@@ -114,7 +119,9 @@ public class DA_TransactionHistory
 
             if (requestModel.Amount > fromAccountBalance)
             {
-                responseModel = Result<TransactionResponseModel>.FailureResult("Insufficient Balance.");
+                responseModel = Result<TransactionResponseModel>.FailureResult(
+                    "Insufficient Balance."
+                );
                 goto result;
             }
 
@@ -123,9 +130,11 @@ public class DA_TransactionHistory
             #region Fetch Transaction History By today date by account no
 
             var today = DateTime.Today;
-            var transactionHistory = await _appDbContext.TransactionHistories
-                .AsNoTracking()
-                .Where(x => x.TransactionDate == today && x.FromAccountNo == requestModel.FromAccountNo)
+            var transactionHistory = await _appDbContext
+                .TransactionHistories.AsNoTracking()
+                .Where(x =>
+                    x.TransactionDate == today && x.FromAccountNo == requestModel.FromAccountNo
+                )
                 .ToListAsync();
 
             decimal totalTransactionAmount = 0;
@@ -142,7 +151,9 @@ public class DA_TransactionHistory
                 limitedBalance = 1000000; // 10 lakhs
                 if (totalTransactionAmount > limitedBalance)
                 {
-                    responseModel = Result<TransactionResponseModel>.FailureResult("You exceed the transfer limit for today.");
+                    responseModel = Result<TransactionResponseModel>.FailureResult(
+                        "You exceed the transfer limit for today."
+                    );
                     goto result;
                 }
             }
@@ -152,7 +163,9 @@ public class DA_TransactionHistory
                 limitedBalance = 3000000; // 30 lakhs
                 if (totalTransactionAmount > limitedBalance)
                 {
-                    responseModel = Result<TransactionResponseModel>.FailureResult("You exceed the transfer limit for today.");
+                    responseModel = Result<TransactionResponseModel>.FailureResult(
+                        "You exceed the transfer limit for today."
+                    );
                     goto result;
                 }
             }
@@ -162,7 +175,9 @@ public class DA_TransactionHistory
                 limitedBalance = 5000000; // 50 lakhs
                 if (totalTransactionAmount > limitedBalance)
                 {
-                    responseModel = Result<TransactionResponseModel>.FailureResult("You exceed the transfer limit for today.");
+                    responseModel = Result<TransactionResponseModel>.FailureResult(
+                        "You exceed the transfer limit for today."
+                    );
                     goto result;
                 }
             }
@@ -177,7 +192,9 @@ public class DA_TransactionHistory
             int fromAccountSavingResult = await _appDbContext.SaveChangesAsync();
             if (fromAccountSavingResult <= 0)
             {
-                responseModel = Result<TransactionResponseModel>.FailureResult("Transferring Fail.");
+                responseModel = Result<TransactionResponseModel>.FailureResult(
+                    "Transferring Fail."
+                );
                 goto result;
             }
 
@@ -191,7 +208,9 @@ public class DA_TransactionHistory
             int toAccountSavingResult = await _appDbContext.SaveChangesAsync();
             if (toAccountSavingResult <= 0)
             {
-                responseModel = Result<TransactionResponseModel>.FailureResult("Transferring Fail.");
+                responseModel = Result<TransactionResponseModel>.FailureResult(
+                    "Transferring Fail."
+                );
                 goto result;
             }
 
@@ -203,22 +222,32 @@ public class DA_TransactionHistory
             int transactionHistorySavingResult = await _appDbContext.SaveChangesAsync();
             if (transactionHistorySavingResult <= 0)
             {
-                responseModel = Result<TransactionResponseModel>.FailureResult("Transferring Fail.");
+                responseModel = Result<TransactionResponseModel>.FailureResult(
+                    "Transferring Fail."
+                );
                 goto result;
             }
 
             #endregion
 
 
-            if (fromAccountSavingResult > 0 && toAccountSavingResult > 0 && transactionHistorySavingResult > 0)
+            if (
+                fromAccountSavingResult > 0
+                && toAccountSavingResult > 0
+                && transactionHistorySavingResult > 0
+            )
             {
                 await transaction.CommitAsync();
-                responseModel = Result<TransactionResponseModel>.SuccessResult(MessageResource.SaveSuccess);
+                responseModel = Result<TransactionResponseModel>.SuccessResult(
+                    MessageResource.SaveSuccess
+                );
                 goto result;
             }
 
             await transaction.RollbackAsync();
-            responseModel = Result<TransactionResponseModel>.FailureResult(MessageResource.SaveFail);
+            responseModel = Result<TransactionResponseModel>.FailureResult(
+                MessageResource.SaveFail
+            );
         }
         catch (Exception ex)
         {
@@ -226,7 +255,7 @@ public class DA_TransactionHistory
             responseModel = Result<TransactionResponseModel>.FailureResult(ex);
         }
 
-    result:
+        result:
         return responseModel;
     }
 
