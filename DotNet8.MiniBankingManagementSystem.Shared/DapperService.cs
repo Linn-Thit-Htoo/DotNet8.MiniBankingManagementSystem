4f15f6c3859a10a73f016c1d2b36ee4f10c11bd7
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 00:38:56 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Shared/DapperService.cs
+++ DotNet8.MiniBankingManagementSystem.Shared/DapperService.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
-using Newtonsoft.Json;
 using Dapper;
 
 namespace DotNet8.MiniBankingManagementSystem.Shared
