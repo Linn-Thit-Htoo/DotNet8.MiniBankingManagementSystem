f2eae8a73815aa509be8b853608f8d2a4ee11219
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-20 21:50:34 +0630

    Add Result Model

---
+++ DotNet8.MiniBankingManagementSystem.Models/Features/Result.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.MiniBankingManagementSystem.Models.Features
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        public static Result<T> SuccessResult(T data, string message = "Operation Successful.")
+        {
+            return new Result<T> { Data = data, Message = message, Success = true };
+        }
+
+        public static Result<T> SuccessResult(string message = "Operation Successful.")
+        {
+            return new Result<T> { Success = true, Message = message };
+        }
+
+        public static Result<T> FailureResult(string message = "Operation Fail.")
+        {
+            return new Result<T> { Success = false, Message = message };
+        }
+
+        public static Result<T> FailureResult(Exception ex)
+        {
+            return new Result<T> { Success = false, Message = ex.ToString() };
+        }
+
+        public Result<T> ExecuteResult(int result)
+        {
+            return result > 0 ? Result<T>.SuccessResult() : Result<T>.FailureResult();
+        }
+    }
+}
