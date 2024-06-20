5991ea3f2be5063d0477ebcbd523c33a6e0c4dff
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 00:38:13 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Api/Features/Reports/TransactionHistoryReport/TransactionHistoryReportController.cs
+++ DotNet8.MiniBankingManagementSystem.Api/Features/Reports/TransactionHistoryReport/TransactionHistoryReportController.cs
@@ -5,39 +5,38 @@ using DotNet8.MiniBankingManagementSystem.Models.Features.TransactionHistory;
 using DotNet8.MiniBankingManagementSystem.Shared;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.MiniBankingManagementSystem.Api.Features.Reports.TransactionHistoryReport
+namespace DotNet8.MiniBankingManagementSystem.Api.Features.Reports.TransactionHistoryReport;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionHistoryReportController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TransactionHistoryReportController : BaseController
+    private readonly DapperService _dapperService;
+
+    public TransactionHistoryReportController(DapperService dapperService)
     {
-        private readonly DapperService _dapperService;
+        _dapperService = dapperService;
+    }
 
-        public TransactionHistoryReportController(DapperService dapperService)
+    [HttpGet("report")]
+    public async Task<IActionResult> ExecuteAsync(string accountNo)
+    {
+        var parameters = new { @FromAccountNo = accountNo };
+        var lst = await _dapperService.QueryAsync<TransactionHistoryReportModel>(
+            "Sp_GetTransactionHistoryListByAccountNo",
+            parameters,
+            CommandType.StoredProcedure
+        );
+
+        var model = new ReportModel<TransactionHistoryReportModel>()
         {
-            _dapperService = dapperService;
-        }
+            ReportFileName = "TransactionHistoryReport",
+            ExportFileName = Guid.NewGuid().ToString(),
+            ReportType = EnumFileType.Pdf,
+            Parameters = null,
+            DataLst = lst
+        };
 
-        [HttpGet("report")]
-        public async Task<IActionResult> ExecuteAsync(string accountNo)
-        {
-            var parameters = new { @FromAccountNo = accountNo };
-            var lst = await _dapperService.QueryAsync<TransactionHistoryReportModel>(
-                "Sp_GetTransactionHistoryListByAccountNo",
-                parameters,
-                CommandType.StoredProcedure
-            );
-
-            var model = new ReportModel<TransactionHistoryReportModel>()
-            {
-                ReportFileName = "TransactionHistoryReport",
-                ExportFileName = Guid.NewGuid().ToString(),
-                ReportType = EnumFileType.Pdf,
-                Parameters = null,
-                DataLst = lst
-            };
-
-            return ExportReport(model);
-        }
+        return ExportReport(model);
     }
 }
