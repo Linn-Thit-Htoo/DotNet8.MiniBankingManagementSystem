a931557fc1825dd8d12a1bc1a4ff86d58d7c23d7
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-20 23:34:06 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Features/Township/TownshipResponseModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Features/Township/TownshipResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.MiniBankingManagementSystem.Models.Features.Township
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Features.Township
 {
     public class TownshipResponseModel { }
 }
