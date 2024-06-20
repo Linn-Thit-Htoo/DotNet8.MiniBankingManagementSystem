e687fc8e2036de833e9cc8dafa5ea7d30c1db200
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-20 23:33:22 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Features/State/StateResponseModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Features/State/StateResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.MiniBankingManagementSystem.Models.Features.State
+﻿namespace DotNet8.MiniBankingManagementSystem.Models.Features.State
 {
     public class StateResponseModel { }
 }
