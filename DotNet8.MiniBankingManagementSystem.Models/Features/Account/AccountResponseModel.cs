a760ff86bf13bb9c889860d8c08a1b1f93bc5ed2
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-20 23:31:55 +0630

    -

--- DotNet8.MiniBankingManagementSystem.Models/Features/Account/AccountResponseModel.cs
+++ DotNet8.MiniBankingManagementSystem.Models/Features/Account/AccountResponseModel.cs
@@ -6,7 +6,5 @@ using System.Threading.Tasks;
 
 namespace DotNet8.MiniBankingManagementSystem.Models.Features.Account
 {
-    public class AccountResponseModel
-    {
-    }
+    public class AccountResponseModel { }
 }
